acf535623b9836659204dae715bb1ed923634f03
Luiz Henrique <luizhjramos@outlook.com>
2021-08-17 06:41:58 -0300

    Aula 11 - função geradora com um contador

--- Funçoes/A011-funcao-geradora/A011-funcao-geradora.js
+++ Funçoes/A011-funcao-geradora/A011-funcao-geradora.js
@@ -20,3 +20,19 @@ const g1 = geradora1()
 for (let valor of g1) {
     console.log(valor)
 }
+
+function* geradora2() {
+    let i = 0
+
+    while(true) {
+        yield i
+        i++
+    }
+}
+
+const g2 = geradora2()
+console.log(g2.next().value)
+console.log(g2.next().value)
+console.log(g2.next().value)
+console.log(g2.next().value)
+console.log(g2.next().value)
