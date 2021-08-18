a0ae52db36f9d1345b6ab9494ea326ebee8924b9
Luiz Henrique <luizhjramos@outlook.com>
2021-08-18 19:09:22 -0300

    Array - usando splice como o POP para remover elementos

---
+++ src/Arrays/A02-metodo-splice/A02-metodo-splice.js
@@ -0,0 +1,7 @@
+
+const nomes = ['Maria', 'Jõao', 'Eduardo', 'Gabriel', 'Júlia']
+
+// usando splice como pop para remover elemento do array
+
+const removidos = nomes.splice(1, 2) // Começando do índice 1 e removendo 2 elementos
+console.log(nomes, removidos)
