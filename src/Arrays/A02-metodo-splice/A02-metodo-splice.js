2e01e6f010d285a0199f63f02bbbc9615a6a0525
Luiz Henrique <luizhjramos@outlook.com>
2021-08-18 19:23:58 -0300

    Array - simulando o unshift com o SPLICE

--- src/Arrays/A02-metodo-splice/A02-metodo-splice.js
+++ src/Arrays/A02-metodo-splice/A02-metodo-splice.js
@@ -15,3 +15,12 @@ const nomes1 = ['Maria', 'Jõao', 'Eduardo', 'Gabriel', 'Júlia']
 const removidos1 = nomes1.splice(2, 1, 'Luiz') // Começando a partir do índice 2 removendo 1 elemento e adicionando 'Luiz' no indice 2
 
 console.log(nomes1) 
+
+
+// Simulando shift
+
+const nomes2 = ['Maria', 'Jõao', 'Eduardo', 'Gabriel', 'Júlia']
+
+const removidos2 = nomes2.splice(0, 1)
+
+console.log(removidos2)
