49af3987c8ba6474d81eb132e9e6d3df35580f3f
Luiz Henrique <luizhjramos@outlook.com>
2021-08-18 19:19:24 -0300

    Array - usando SPLICE como POP e PUSH.

--- src/Arrays/A02-metodo-splice/A02-metodo-splice.js
+++ src/Arrays/A02-metodo-splice/A02-metodo-splice.js
@@ -1,7 +1,17 @@
 
 const nomes = ['Maria', 'Jõao', 'Eduardo', 'Gabriel', 'Júlia']
 
+
 // usando splice como pop para remover elemento do array
 
 const removidos = nomes.splice(1, 2) // Começando do índice 1 e removendo 2 elementos
 console.log(nomes, removidos)
+
+
+// splice(índice, delete, elem1, elem2, elem3)
+
+const nomes1 = ['Maria', 'Jõao', 'Eduardo', 'Gabriel', 'Júlia']
+
+const removidos1 = nomes1.splice(2, 1, 'Luiz') // Começando a partir do índice 2 removendo 1 elemento e adicionando 'Luiz' no indice 2
+
+console.log(nomes1) 
