33be3e09d0f86dd09b8725949215cde04375f2a0
Luiz Henrique <luizhjramos@outlook.com>
2021-08-22 22:52:08 -0300

    A04 - filter

--- src/Arrays/A04-filter/A04-filter.js
+++ src/Arrays/A04-filter/A04-filter.js
@@ -17,3 +17,18 @@ console.log(numerosFiltradosTradicional)
 const numerosFiltrados = numeros.filter((num) => num >= 10 )
 
 console.log(numerosFiltrados)
+
+
+// Filtrando um Array de objetos
+// Retorne as pessoas que tem o nome com 5 letras ou mais
+// Retorne as pessoas com mais de 50 anos
+// Retorne as pessoas cujo nome termina com "a"
+
+const pessoas = [
+    { nome: 'Luiz', idade: 62 },
+    { nome: 'Maria', idade: 23 },
+    { nome: 'Eduardo', idade: 55 },
+    { nome: 'Letícia', idade: 19 },
+    { nome: 'Rosana', idade: 32 },
+    { nome: 'Wallace', idade: 47 },
+]
