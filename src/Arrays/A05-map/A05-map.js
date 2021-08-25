d09c57748939930b760a1b0efd7704c37fc7914e
Luiz Henrique <luizhjramos@outlook.com>
2021-08-24 22:52:08 -0300

    A05 Map - Retornando novo array só com uma chave

--- src/Arrays/A05-map/A05-map.js
+++ src/Arrays/A05-map/A05-map.js
@@ -5,3 +5,21 @@ const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
 const numerosEmDobro = numeros.map(valor => valor * 2)
 
 console.log(numerosEmDobro)
+
+
+// Para cada elemento:
+// Retorne apenas uma string com nome da pessoa
+// Remova a chave "nome" do objeto
+// Adicione uma chave id em cada objeto
+
+const pessoas = [
+    { nome: 'Luiz', idade: 62 },
+    { nome: 'Maria', idade: 23 },
+    { nome: 'Eduardo', idade: 55 },
+    { nome: 'Letícia', idade: 19 },
+    { nome: 'Rosana', idade: 32 },
+    { nome: 'Wallace', idade: 47 },
+]
+
+const retornaStringComNome = pessoas.map( pessoa => pessoa.nome ) 
+console.log(retornaStringComNome)
