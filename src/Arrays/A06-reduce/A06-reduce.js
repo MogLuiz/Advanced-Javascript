d33db876aa9bb7d4e17567e967d5cb39a7de275a
Luiz Henrique <luizhjramos@outlook.com>
2021-08-26 23:37:48 -0300

    A06 - reduce

--- src/Arrays/A06-reduce/A06-reduce.js
+++ src/Arrays/A06-reduce/A06-reduce.js
@@ -4,7 +4,7 @@
 
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
 
-const total = numeros.reduce(function(acumulador, valor, indice, array) {
+const total = numeros.reduce(function (acumulador, valor, indice, array) {
     acumulador += valor
     return acumulador
 }, 0)
@@ -12,17 +12,38 @@ const total = numeros.reduce(function(acumulador, valor, indice, array) {
 console.log(total)
 
 
-const arrayPares = numeros.reduce(function(acumulador, valor, indice, array) {
-    if(valor % 2 === 0) acumulador.push(valor)
+const arrayPares = numeros.reduce(function (acumulador, valor, indice, array) {
+    if (valor % 2 === 0) acumulador.push(valor)
     return acumulador
 }, [])
 
 console.log(arrayPares)
 
 
-const arrayDobroValores = numeros.reduce(function(acumulador, valor, indice, array) {
+const arrayDobroValores = numeros.reduce(function (acumulador, valor, indice, array) {
     acumulador.push(valor * 2)
     return acumulador
 }, [])
 
 console.log(arrayDobroValores)
+
+
+// Retorne a pessoa mais velha
+
+const pessoas = [
+    { nome: 'Luiz', idade: 62 },
+    { nome: 'Maria', idade: 23 },
+    { nome: 'Eduardo', idade: 55 },
+    { nome: 'Letícia', idade: 19 },
+    { nome: 'Rosana', idade: 32 },
+    { nome: 'Wallace', idade: 47 },
+]
+
+const pessoas = [
+    { nome: 'Luiz', idade: 62 },
+    { nome: 'Maria', idade: 23 },
+    { nome: 'Eduardo', idade: 55 },
+    { nome: 'Letícia', idade: 19 },
+    { nome: 'Rosana', idade: 32 },
+    { nome: 'Wallace', idade: 47 },
+]
