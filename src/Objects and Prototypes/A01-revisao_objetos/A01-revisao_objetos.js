38b92f74e8bf4eae4ae456648764244df20735f7
Luiz Henrique <luizhjramos@outlook.com>
2021-08-28 15:53:13 -0300

    A01 - Criando e manipulando objetos com construtor

--- src/Objects and Prototypes/A01-revisao_objetos/A01-revisao_objetos.js
+++ src/Objects and Prototypes/A01-revisao_objetos/A01-revisao_objetos.js
@@ -1,3 +1,4 @@
+// Objeto Literal
 const pessoa = {
     nome: 'Luiz',
     sobrenome: 'Henrique',
@@ -14,3 +15,29 @@ console.log(pessoa.sobrenome)
 const chave = 'nome'
 
 console.log(pessoa[chave])
+
+
+console.log('----------------------')
+
+// Construtor do objeto
+const pessoa2 = new Object()
+
+
+pessoa2.name = 'Luizin'
+pessoa2.sobrenome = 'DúGrau'
+pessoa2.idade = 21
+
+pessoa2.falarNome = function() {
+   return `${this.name} está falando seu nome`
+}
+
+pessoa2.getDataNascimento = function() {
+    const dataAtual = new Date()
+
+    return dataAtual.getFullYear() - this.idade
+}
+
+delete pessoa2.sobrenome
+
+console.log(pessoa2.falarNome())
+console.log(pessoa2.getDataNascimento())
