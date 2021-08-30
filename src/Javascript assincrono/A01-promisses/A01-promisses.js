d48ff5f16b9f8fa99e78d8faf316d4faf777c746
Luiz Henrique <luizhjramos@outlook.com>
2021-08-30 19:27:03 -0300

    Promisses - tratando erro com o Catch e o Reject

--- src/Javascript assincrono/A01-promisses/A01-promisses.js
+++ src/Javascript assincrono/A01-promisses/A01-promisses.js
@@ -5,8 +5,9 @@ function rand(min, max) {
     return Math.floor(Math.random() * (max - min) + min)
 }
 
-function esperaAi(msg, tempo) {
+function esperaAi(msg, tempo) {  
    return new Promise((resolve, reject) => {
+    if(typeof msg !== 'string') reject('BAD VALUE')
     setTimeout(() => {
         resolve(msg)
     }, tempo)
@@ -17,8 +18,11 @@ function esperaAi(msg, tempo) {
 esperaAi('Frase 1', rand(1, 3))
     .then(res => {
         console.log(res)
-        return esperaAi('Frase 2', rand(1, 3))
+        return esperaAi(234234, rand(1, 3))
     })
     .then(res2 => {
         console.log(res2)
+    })
+    .catch(e => {
+        console.log('ERRO', e)
     })
